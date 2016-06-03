ff30ba648ae2917967af71c0c3e1fa628a7fa472
Ruairi O Brien <ruairitobrien@gmail.com>
2016-06-03 09:25:19 +0100

    babeled

--- SSH.js
+++ SSH.js
@@ -1,207 +1,269 @@
-// @Compiler-Transpile "true"
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); // @Compiler-Transpile "true"
 // @Compiler-Output "../Dist/SSH.js"
 
-import {promisify} from 'js-toolkit'
+var _jsToolkit = require('js-toolkit');
 
-const Driver = require('ssh2')
-const FS = require('fs')
-const Path = require('path')
-const Escape = require('shell-escape')
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-const access = promisify(FS.access)
-const readFile = promisify(FS.readFile)
+var Driver = require('ssh2');
+var FS = require('fs');
+var Path = require('path');
+var Escape = require('shell-escape');
 
-const validStreams = new Set(['stdout', 'stderr', 'both'])
+var access = (0, _jsToolkit.promisify)(FS.access);
+var readFile = (0, _jsToolkit.promisify)(FS.readFile);
 
-export default class SSH {
-  constructor() {
-    this.connection = null
-    this.connected = false
+var validStreams = new Set(['stdout', 'stderr', 'both']);
+
+var SSH = function () {
+  function SSH() {
+    _classCallCheck(this, SSH);
+
+    this.connection = null;
+    this.connected = false;
   }
-  connect(config) {
-    this.connection = new Driver()
-    return new Promise((resolve, reject) => {
-      if (typeof config.username !== 'string') {
-        throw new Error('No username provided')
-      } else if (typeof config.host !== 'string') {
-        throw new Error('No host provided')
-      }
-      if (config.privateKey) {
-        if (Path.isAbsolute(config.privateKey)) {
-          try {
-            config.privateKey = FS.readFileSync(config.privateKey)
-          } catch (err) {
-            throw new Error('Unable to read private key')
+
+  _createClass(SSH, [{
+    key: 'connect',
+    value: function connect(config) {
+      var _this = this;
+
+      this.connection = new Driver();
+      return new Promise(function (resolve, reject) {
+        if (typeof config.username !== 'string') {
+          throw new Error('No username provided');
+        } else if (typeof config.host !== 'string') {
+          throw new Error('No host provided');
+        }
+        if (config.privateKey) {
+          if (Path.isAbsolute(config.privateKey)) {
+            try {
+              config.privateKey = FS.readFileSync(config.privateKey);
+            } catch (err) {
+              throw new Error('Unable to read private key');
+            }
           }
         }
+        _this.connection.on('error', reject);
+        _this.connection.on('ready', function () {
+          _this.connected = true;
+          _this.connection.removeListener('error', reject);
+          resolve(_this);
+        });
+        _this.connection.connect(config);
+      });
+    }
+  }, {
+    key: 'mkdir',
+    value: function mkdir(path) {
+      if (!this.connected) {
+        throw new Error('SSH Not yet connected');
       }
-      this.connection.on('error', reject)
-      this.connection.on('ready', () => {
-        this.connected = true
-        this.connection.removeListener('error', reject)
-        resolve(this)
-      })
-      this.connection.connect(config)
-    })
-  }
-  mkdir(path) {
-    if (!this.connected) {
-      throw new Error('SSH Not yet connected')
+      return this.exec('mkdir', ['-p', path]);
     }
-    return this.exec('mkdir', ['-p', path])
-  }
-  exec(filePath, args = [], options = {}) {
-    if (!this.connected) {
-      throw new Error('SSH Not yet connected')
-    }
-    if (typeof filePath !== 'string') {
-      throw new Error('Executable Path must be a string')
-    } else if (!(args instanceof Array)) {
-      throw new Error('args must be an array')
-    } else if (typeof options !== 'object') {
-      throw new Error('Options must be an object')
-    } else if (options.cwd && typeof options.cwd !== 'string') {
-      throw new Error('Options.cwd must be a string')
-    } else if (options.stdin && typeof options.stdin !== 'string') {
-      throw new Error('Options.stdin must be a string')
-    }
-    options.stream = validStreams.has(options.stream) ? options.stream : 'stdout'
-    return this.execCommand([filePath].concat(Escape(args)).join(' '), options).then(({stdout, stderr, code, signal}) => {
-      if (options.stream === 'both') {
-        return {stderr, stdout, code, signal}
-      } else if (options.stream === 'stderr') {
-        return stderr
-      } else if (options.stream === 'stdout') {
-        if (stderr.length) {
-          throw new Error(stderr)
-        } else return stdout
+  }, {
+    key: 'exec',
+    value: function exec(filePath) {
+      var args = arguments.length <= 1 || arguments[1] === undefined ? [] : arguments[1];
+      var options = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
+
+      if (!this.connected) {
+        throw new Error('SSH Not yet connected');
       }
-    })
-  }
-  execCommand(command, options = {}) {
-    if (!this.connected) {
-      throw new Error('SSH Not yet connected')
-    }
-    if (typeof command !== 'string') {
-      throw new Error('Command must be a string')
-    } else if (typeof options !== 'object') {
-      throw new Error('Options must be an object')
-    } else if (options.cwd && typeof options.cwd !== 'string') {
-      throw new Error('Options.cwd must be a string')
-    } else if (options.stdin && typeof options.stdin !== 'string') {
-      throw new Error('Options.stdin must be a string')
-    }
-    if (options.cwd) {
-      command = 'cd ' + Escape([options.cwd]) + ' ; ' + command
-    }
-    return new Promise((resolve, reject) => {
-      this.connection.exec(command, function(err, stream) {
-        if (err) {
-          return reject(err)
+      if (typeof filePath !== 'string') {
+        throw new Error('Executable Path must be a string');
+      } else if (!(args instanceof Array)) {
+        throw new Error('args must be an array');
+      } else if ((typeof options === 'undefined' ? 'undefined' : _typeof(options)) !== 'object') {
+        throw new Error('Options must be an object');
+      } else if (options.cwd && typeof options.cwd !== 'string') {
+        throw new Error('Options.cwd must be a string');
+      } else if (options.stdin && typeof options.stdin !== 'string') {
+        throw new Error('Options.stdin must be a string');
+      }
+      options.stream = validStreams.has(options.stream) ? options.stream : 'stdout';
+      return this.execCommand([filePath].concat(Escape(args)).join(' '), options).then(function (_ref) {
+        var stdout = _ref.stdout;
+        var stderr = _ref.stderr;
+        var code = _ref.code;
+        var signal = _ref.signal;
+
+        if (options.stream === 'both') {
+          return { stderr: stderr, stdout: stdout, code: code, signal: signal };
+        } else if (options.stream === 'stderr') {
+          return stderr;
+        } else if (options.stream === 'stdout') {
+          if (stderr.length) {
+            throw new Error(stderr);
+          } else return stdout;
         }
-        const contents = {stdout: [], stderr: []}
-        stream.on('close', function(code, signal) {
-          resolve({stdout: contents.stdout.join(''), stderr: contents.stderr.join(''), code, signal})
-        }).on('data', function(data) {
-          contents.stdout.push(data)
-        }).stderr.on('data', function(data) {
-          contents.stderr.push(data)
-        })
-        if (options.stdin) {
-          stream.write(options.stdin)
-          stream.end()
-        }
-      })
-    })
-  }
-  put(localFile, remoteFile, SFTP, retry = true) {
-    if (!this.connected) {
-      throw new Error('SSH Not yet connected')
-    } else if (typeof localFile !== 'string') {
-      throw new Error('localFile must be a string')
-    } else if (typeof remoteFile !== 'string') {
-      throw new Error('remoteFile must be a string')
+      });
     }
-    return access(localFile, FS.R_OK).catch(() => {
-      throw new Error(`Local file ${localFile} doesn't exist`)
-    }).then(() => {
-      return SFTP ? Promise.resolve(SFTP) : this.requestSFTP()
-    }).then(SFTP => {
-      return new Promise((resolve, reject) => {
-        SFTP.fastPut(localFile, remoteFile, (err) => {
-          if (!err) {
-            return resolve()
-          }
-          if (err.message === 'No such file' && retry) {
-            resolve(this.mkdir(Path.dirname(remoteFile)).then(() =>
-              this.put(localFile, remoteFile, SFTP, false)
-            ))
-          } else reject(err)
-        })
-      })
-    })
-  }
-  putMulti(files, SFTP) {
-    if (!this.connected) {
-      throw new Error('SSH Not yet connected')
-    } else if (!(files instanceof Array)) {
-      throw new Error('Files must be an array')
-    }
-    SFTP = SFTP ? Promise.resolve(SFTP) : this.requestSFTP()
-    return SFTP.then(SFTP => {
-      const Promises = []
-      files.forEach(file => {
-        Promises.push(this.put(file.Local, file.Remote, SFTP))
-      })
-      return Promise.all(Promises)
-    })
-  }
-  get(remoteFile, localFile, SFTP) {
-    if (!this.connected) {
-      throw new Error('SSH Not yet connected')
-    } else if (typeof remoteFile !== 'string') {
-      throw new Error('remoteFile must be a string')
-    } else if (typeof localFile !== 'string') {
-      throw new Error('localFile must be a string')
-    }
-    SFTP = SFTP ? Promise.resolve(SFTP) : this.requestSFTP()
-    return SFTP.then(SFTP => {
-      return new Promise(function(resolve, reject) {
-        SFTP.fastGet(localFile, remoteFile, function(err){
+  }, {
+    key: 'execCommand',
+    value: function execCommand(command) {
+      var _this2 = this;
+
+      var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+      if (!this.connected) {
+        throw new Error('SSH Not yet connected');
+      }
+      if (typeof command !== 'string') {
+        throw new Error('Command must be a string');
+      } else if ((typeof options === 'undefined' ? 'undefined' : _typeof(options)) !== 'object') {
+        throw new Error('Options must be an object');
+      } else if (options.cwd && typeof options.cwd !== 'string') {
+        throw new Error('Options.cwd must be a string');
+      } else if (options.stdin && typeof options.stdin !== 'string') {
+        throw new Error('Options.stdin must be a string');
+      }
+      if (options.cwd) {
+        command = 'cd ' + Escape([options.cwd]) + ' ; ' + command;
+      }
+      return new Promise(function (resolve, reject) {
+        _this2.connection.exec(command, function (err, stream) {
           if (err) {
-            reject(err)
-          } else resolve()
-        })
-      })
-    })
-  }
-  requestSFTP() {
-    if (!this.connected) {
-      throw new Error('SSH Not yet connected')
+            return reject(err);
+          }
+          var contents = { stdout: [], stderr: [] };
+          stream.on('close', function (code, signal) {
+            resolve({ stdout: contents.stdout.join(''), stderr: contents.stderr.join(''), code: code, signal: signal });
+          }).on('data', function (data) {
+            contents.stdout.push(data);
+          }).stderr.on('data', function (data) {
+            contents.stderr.push(data);
+          });
+          if (options.stdin) {
+            stream.write(options.stdin);
+            stream.end();
+          }
+        });
+      });
     }
-    return new Promise((resolve, reject) => {
-      this.connection.sftp(function(err, sftp) {
-        if (err) {
-          reject(err)
-        } else resolve(sftp)
-      })
-    })
-  }
-  requestShell() {
-    if (!this.connected) {
-      throw new Error('SSH Not yet connected')
+  }, {
+    key: 'put',
+    value: function put(localFile, remoteFile, SFTP) {
+      var _this3 = this;
+
+      var retry = arguments.length <= 3 || arguments[3] === undefined ? true : arguments[3];
+
+      if (!this.connected) {
+        throw new Error('SSH Not yet connected');
+      } else if (typeof localFile !== 'string') {
+        throw new Error('localFile must be a string');
+      } else if (typeof remoteFile !== 'string') {
+        throw new Error('remoteFile must be a string');
+      }
+      return access(localFile, FS.R_OK).catch(function () {
+        throw new Error('Local file ' + localFile + ' doesn\'t exist');
+      }).then(function () {
+        return SFTP ? Promise.resolve(SFTP) : _this3.requestSFTP();
+      }).then(function (SFTP) {
+        return new Promise(function (resolve, reject) {
+          SFTP.fastPut(localFile, remoteFile, function (err) {
+            if (!err) {
+              return resolve();
+            }
+            if (err.message === 'No such file' && retry) {
+              resolve(_this3.mkdir(Path.dirname(remoteFile)).then(function () {
+                return _this3.put(localFile, remoteFile, SFTP, false);
+              }));
+            } else reject(err);
+          });
+        });
+      });
     }
-    return new Promise((resolve, reject) => {
-      this.connection.shell(function(err, shell) {
-        if (err) {
-          reject(err)
-        } else resolve(shell)
-      })
-    })
-  }
-  end() {
-    this.connection.end()
-    this.connection = null
-    this.connected = false
-  }
-}
+  }, {
+    key: 'putMulti',
+    value: function putMulti(files, SFTP) {
+      var _this4 = this;
+
+      if (!this.connected) {
+        throw new Error('SSH Not yet connected');
+      } else if (!(files instanceof Array)) {
+        throw new Error('Files must be an array');
+      }
+      SFTP = SFTP ? Promise.resolve(SFTP) : this.requestSFTP();
+      return SFTP.then(function (SFTP) {
+        var Promises = [];
+        files.forEach(function (file) {
+          Promises.push(_this4.put(file.Local, file.Remote, SFTP));
+        });
+        return Promise.all(Promises);
+      });
+    }
+  }, {
+    key: 'get',
+    value: function get(remoteFile, localFile, SFTP) {
+      if (!this.connected) {
+        throw new Error('SSH Not yet connected');
+      } else if (typeof remoteFile !== 'string') {
+        throw new Error('remoteFile must be a string');
+      } else if (typeof localFile !== 'string') {
+        throw new Error('localFile must be a string');
+      }
+      SFTP = SFTP ? Promise.resolve(SFTP) : this.requestSFTP();
+      return SFTP.then(function (SFTP) {
+        return new Promise(function (resolve, reject) {
+          SFTP.fastGet(localFile, remoteFile, function (err) {
+            if (err) {
+              reject(err);
+            } else resolve();
+          });
+        });
+      });
+    }
+  }, {
+    key: 'requestSFTP',
+    value: function requestSFTP() {
+      var _this5 = this;
+
+      if (!this.connected) {
+        throw new Error('SSH Not yet connected');
+      }
+      return new Promise(function (resolve, reject) {
+        _this5.connection.sftp(function (err, sftp) {
+          if (err) {
+            reject(err);
+          } else resolve(sftp);
+        });
+      });
+    }
+  }, {
+    key: 'requestShell',
+    value: function requestShell() {
+      var _this6 = this;
+
+      if (!this.connected) {
+        throw new Error('SSH Not yet connected');
+      }
+      return new Promise(function (resolve, reject) {
+        _this6.connection.shell(function (err, shell) {
+          if (err) {
+            reject(err);
+          } else resolve(shell);
+        });
+      });
+    }
+  }, {
+    key: 'end',
+    value: function end() {
+      this.connection.end();
+      this.connection = null;
+      this.connected = false;
+    }
+  }]);
+
+  return SSH;
+}();
+
+exports.default = SSH;
